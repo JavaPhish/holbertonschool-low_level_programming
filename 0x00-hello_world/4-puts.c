32c62cd03b2be73b811716a7a956e63e112a852d
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-19 13:09:17 -0400

    Updated to comply with betty standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-/*Testing this garbage format*/
-
 #include<stdio.h>
 #include<string.h>
-
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts (str);
-  return (0);
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
