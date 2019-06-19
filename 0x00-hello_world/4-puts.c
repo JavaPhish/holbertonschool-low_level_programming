835d620b56e45111113aef35d1455ae12e0ff984
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-19 12:27:22 -0400

    Updated 4 to comply with betty standards

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<string.h>
-int main() {
+int main (void)
+{
   char str[] = "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return 0;
+  puts (str);
+  return (0);
 }
