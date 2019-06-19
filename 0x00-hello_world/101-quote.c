e5e89b2c3dcf16639f4b8aa937031abf5b9c8082
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-19 14:50:49 -0400

    Created and added 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+fprintf(stderr, "%s", text[]);
+return (0);
+}
