9828c957b41cb365c3c966c33afdcbf92ad68c22
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-19 12:31:16 -0400

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 #include<stdio.h>
 #include<string.h>
+
 int main (void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle\n";
+  char str[] = "\"Programming is like building a multilingual puzzle";
   puts (str);
   return (0);
 }
