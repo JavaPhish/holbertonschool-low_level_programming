77adf05a3abe3324747f829abfac5da11e719591
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-20 19:06:44 -0400

    Created and added 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cheese[2];
+	int num = 48;
+	int loop1;
+	int loop2;
+	int putVar = 0;
+
+	cheese[0] = 44;
+	cheese[1] = 32;
+
+
+	while (putVar < 2)
+	{
+		putchar('0');
+		putVar++;
+	}
+	putVar = 0;
+	for (loop1 = 1; loop1 < 10; loop1++)
+	{
+		for (loop2 = 0; loop2 < 10; loop2++)
+		{
+			while (putVar < 2)
+			{
+				putchar(cheese[putVar]);
+				putVar++;
+			}
+			putchar(loop1 + 48);
+			putchar(loop2 + 48);
+			putVar = 0;
+		}
+	}
+
+
+	putchar('\n');
+}
