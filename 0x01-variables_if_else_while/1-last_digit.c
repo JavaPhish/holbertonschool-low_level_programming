50ec7691f7c6aa35b732e55c0de5e5c26645b1bc
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-20 13:26:55 -0400

    No longer causes a seg fault

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,15 +10,16 @@
 int main(void)
 {
 int n;
+int endN = n % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%s%d%s%d", "Last digit of ", n, " is ", n % 10);
-	if ((10 % n) > 5)
+	printf("%s%d%s%d", "Last digit of ", n, " is ", endN);
+	if (endN > 5)
 	{
 		printf("%s", " and is greater than 5\n");
 	}
-	else if ((10 % n) < 6 && (10 % n) != 0)
+	else if ((endN < 6) && (endN != 0))
 	{
 		printf("%s", " and is less than 6 and not 0\n");
 	}
