6bcf67fd94f4035d011bd2a264c1e12b1b0a0127
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-20 10:38:04 -0400

    Created and added 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+	putchar(i);
+	}
+	for (int x = 65; x <= 90; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
