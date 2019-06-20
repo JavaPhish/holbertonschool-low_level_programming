ccf2eaa3398add0218c5bbb2164e7200bf6f70c0
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-20 10:33:06 -0400

    Created and added 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		if (!(i == 101 || i == 113))
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
