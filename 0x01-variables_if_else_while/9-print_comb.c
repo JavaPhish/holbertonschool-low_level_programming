b1eae2b5dd43da9f45c1e33a5c9d0e289ff35e18
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-20 16:56:39 -0400

    Fixed 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,15 @@
 int main(void)
 {
 	int cheese[2];
-
+	int num = 48;
+	int i;
 	cheese[0] = 44;
 	cheese[1] = 32;
-	int num = 48;
 
 	putchar(num);
 	num++;
 	while (num < 58)
 	{
-		int i = 0;
-
 		while (i < 2)
 		{
 		putchar(cheese[i]);
