dc90d4f65ad2b3ddb729d93ce2d1b2cb10519b3b
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-25 10:38:20 -0400

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - Returns if a char is a alpha value or not
+ * @c: the char to be evaluated
+ *
+ * Return: 0 (not an alpha character)
+ * Reutrn: 1 (is an alpha character)
+ */
 int _isalpha(int c)
 {
 	if ((c < 91 && c > 64) || (c < 122 && c > 96))
