731689fe2ea8d6b11dd041eebafff85e6cdc7526
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-25 10:21:35 -0400

    Using embedded for loops to print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,15 @@
 void print_alphabet_x10(void)
 {
 	int printAmount = 10;
-	int loop;
+	int loop1;
+	int loop2;
 
-	for (loop = 1; loop <= printAmount; loop++)
+	for (loop1 = 1; loop1 <= printAmount; loop1++)
 	{
-		print_alphabet();
+		for (loop2 = 'a'; loop2 <= 'z'; loop2++)
+		{
+			_putchar(loop2);
+		}
+			_putchar('\n');
 	}
 }
