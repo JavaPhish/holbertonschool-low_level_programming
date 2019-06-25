190461bf7e8493b04662e25df76c6cc17701f2f7
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-25 12:54:57 -0400

    8 prints out a full day minute by minute using 4 nested for loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+
+#include "holberton.h"
+
+
+void jack_bauer(void)
+{
+int loop1, loop2, loop3, loop4;
+
+	for (loop1 = 0; loop1 < 3 ; loop1++)
+	{
+		for (loop2 = 0; loop2 < 4; loop2++)
+		{
+			for (loop3 = 0; loop3 < 6; loop3++)
+			{
+				for (loop4 = 0; loop4 < 10; loop4++)
+				{
+					_putchar(loop1 + 48);
+					_putchar(loop2 + 48);
+					_putchar(':');
+					_putchar(loop3 + 48);
+					_putchar(loop4 + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
