f62b60d39a08b15ff0749d0daf4f93c4d8436e91
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-25 09:15:11 -0400

    Created and added 0 0 prints the word "Holberton" to console by using a custom putchar and a for loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include"holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "Holberton";
+	int loop = 0;
+
+	for (; loop < 9; loop++)
+	{
+		_putchar(word[loop]);
+	}
+		_putchar('\n');
+
+	return (0);
+}
