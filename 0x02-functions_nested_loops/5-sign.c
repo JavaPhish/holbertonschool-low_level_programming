0bee4adf15fcb0728d5a8897f8b13e2dd00cc0de
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-25 10:45:47 -0400

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * print_sign - determines if a number is greater than, less than or equal to zero
+ * @n: the number to be evaluated
+ * 
+ * Return: 0 (is zero)
+ * Return: 1 (Greater than zero)
+ * Return: -1 (less than zero)
+ */
 int print_sign(int n)
 {
 	if ( n > 0 )
