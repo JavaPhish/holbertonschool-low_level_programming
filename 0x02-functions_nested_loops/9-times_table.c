d2d8b5f9919f034ab562917db07ec87cec856d2e
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-25 13:32:36 -0400

    Created 9 which prints the times table to console

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+void times_table(void)
+{
+	int loop1, loop2, temp;
+
+	for (loop1 = 0; loop1 < 10; loop1++)
+	{
+		_putchar('0');
+		for (loop2 = 1; loop2 < 10; loop2++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			temp = loop1 * loop2;
+			if (temp > 99)
+			{
+				temp = temp - 100;
+				_putchar('1');
+			}
+			if (temp > 9)
+			{
+				_putchar(((temp - (temp % 10)) / 10) + 48);
+				temp = temp % 10;
+			}
+			else
+			{
+				_putchar(' ');
+			}
+				_putchar(temp + 48);
+		}
+		_putchar('\n');
+	}
+}
