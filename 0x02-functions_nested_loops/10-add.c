a2ff764ec64824787f3ff2c7a2af2c08d060a3e3
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-25 15:35:11 -0400

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+
+/**
+ * add - Prints the times table to console
+ * @i: number 1
+ * @x: number 2
+ * Return: the added numbers value
+ */
 int add(int i, int x)
 {
 	return (i + x);
