581d2cae448500b440aa612a4898f81b05cf9c9d
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-25 11:04:51 -0400

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+ * print_last_digit - prints the last decimal
+ * @i: the number to be printed from
+ * Return: the last digit
+ */
 int print_last_digit(int i)
 {
 	return (i % 10);
