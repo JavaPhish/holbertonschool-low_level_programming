f24386cdc28f3408a6d62a6a6e320213f15a1dcf
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-27 11:10:39 -0400

    10 prints a triangle starting at the right hand side at the specified length

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+
+/**
+ * print_triangle - prints a triangle starting at the right hand corner
+ *
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int loop1, loop2, loop3;
+
+	if (size > 0)
+	{
+
+		for (loop1 = 0; loop1 < size; loop1++)
+		{
+
+			for (loop3 = 0; loop3 < size - loop1; loop3++)
+			{
+				_putchar(' ');
+			}
+			for (loop2 = 0; loop2 + loop3 < size  ; loop2++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
