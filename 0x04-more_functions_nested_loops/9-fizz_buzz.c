b79d4eb742777a64aef5fe8df5720acc6e680074
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-27 10:41:20 -0400

    9 prints 0 - 100 but replaces multiples of 3 with fizz, multiples of 5 with buzz and multiples of both 3 and 5 with fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include "stdio.h"
+
+/**
+ * fizz_buzz - prints 1 - 100 and fizz and buzz in place of 3 and 5 multiples
+ */
+
+void fizz_buzz(void)
+{
+	int loop1;
+
+	for (loop1 = 1; loop1 < 101; loop1++)
+	{
+		if ((loop1 % 3) == 0 && (loop1 % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((loop1 % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((loop1 % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d%s", loop1, " ");
+		}
+	}
+
+	putchar('\n');
+
+}
