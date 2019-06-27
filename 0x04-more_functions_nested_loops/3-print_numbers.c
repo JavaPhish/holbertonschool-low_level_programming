a584b057769757b763d5d4a035ca625c6f456774
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-27 09:40:52 -0400

    3 prints numbers 0 - 9 only using two _putchars

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints 0 through 9 using only two putchars
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i + 48);
+	}
+
+	_putchar('\n');
+}
