93d7307f4828f5c9e60f48918db8c5d46cc8262f
JavaPhish <31843656+JavaPhish@users.noreply.github.com>
2019-06-26 23:16:11 -0400

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,36 +19,3 @@ void print_to_98(int n)
 	}
 	_putchar('\n');
 }
-
-void printNum(int i)
-{
-	if (i > 99)
-	{
-		_putchar(i / 100);
-		i = i - ((i / 100) * 100);
-	}
-
-	if (i < 0)
-	{
-		if (-i > 9)
-		{
-			_putchar('-');
-			_putchar((-i / 10) + 48);
-			_putchar((-i % 10) + 48);
-		}
-		else
-		{
-			_putchar(-i + 48);
-		}
-
-	}
-	else if (i > 9)
-	{
-		_putchar((i / 10) + 48);
-		_putchar((i % 10) + 48);
-	}
-	else
-	{
-		_putchar(i + 48);
-	}
-}
