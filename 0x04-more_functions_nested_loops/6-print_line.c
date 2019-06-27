a8e3c2eb65d1634c31938f3c541717963f2b8a0e
CARTER_CLEMENTS <831@holbertonschool.com>
2019-06-27 10:08:56 -0400

    6 prints a line a i specified length

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line: prints a line a i length
+ *
+ * @i: the length of the line
+ */
+
+void print_line(int i)
+{
+
+	for (; i > 0; i--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
