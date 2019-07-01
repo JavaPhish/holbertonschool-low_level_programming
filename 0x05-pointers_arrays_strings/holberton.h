5a83e9397899a591202d18c0c56f48180d1ec154
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-01 14:30:15 -0400

    Updated header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
@@ -8,3 +11,6 @@ int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _putchar(char c);
+
+
+#endif
