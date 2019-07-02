832fc7db280b7949875fb53e5644d3999673bf91
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-02 08:58:41 -0400

    4 Prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+
+int _strlen(char *s);
 /**
  * print_rev - prints a string reversed
  *
@@ -10,10 +12,31 @@ void print_rev(char *s)
 	int temp;
 
 	temp = _strlen(s);
-	while (temp != 0)
+	while (temp > -1)
 	{
 		_putchar(s[temp]);
 		temp--;
 	}
 	_putchar('\n');
+}
+
+/**
+ *_strlen - copied from 0x02
+ * @s: the pointer to a string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int temp;
+
+	temp = 0;
+
+	while (s[temp] != '\0')
+	{
+		temp++;
+	}
+
+	return (temp);
+
 }
