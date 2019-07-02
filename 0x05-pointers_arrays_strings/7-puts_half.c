f95c441e71cccd0234bf1afe3ba40fe436d2b80c
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-02 10:42:38 -0400

    7 prints the last half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 
+int _strlen(char *s);
 /**
  * puts_half - Prints the last half of a number
  * @str: the string to be printed
@@ -8,18 +9,43 @@ void puts_half(char *str)
 {
 	int temp, printNum;
 
+	temp = 0;
 	if (_strlen(str) % 2 != 0)
 	{
 		printNum = (_strlen(str) - 1) / 2;
+	}
+	else
+	{
+		printNum = (_strlen(str) / 2);
 	}
 	while (str[temp] != '\0')
 	{
 		if (temp >= printNum)
 		{
-		_putchar(str[temp]);
+			_putchar(str[temp]);
 		}
 	temp++;
 	}
 
 	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int temp;
+
+	temp = 0;
+	while (s[temp] != '\0')
+	{
+		temp++;
+	}
+
+	return (temp);
+
 }
