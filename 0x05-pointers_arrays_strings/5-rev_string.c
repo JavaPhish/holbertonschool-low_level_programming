cd4538d584c2d7eb35615964b80a309baf9b998a
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-02 09:03:31 -0400

    Reverses an array

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-
+int _strlen(char *s);
 /**
  * rev_string - reverses a string in variable
  * @s: The string to be reversed
@@ -23,4 +23,25 @@ void rev_string(char *s)
 		s[_strlen(s) - temp] = str[temp - 1];
 		temp--;
 	}
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the pointer to a string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int temp;
+
+	temp = 0;
+
+	while (s[temp] != '\0')
+	{
+		temp++;
+	}
+
+	return (temp);
 }
