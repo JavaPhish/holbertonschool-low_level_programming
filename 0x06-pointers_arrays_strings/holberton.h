8cc19c6388db29f259b207cbc283566bf904c400
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-07 16:23:47 -0400

    Updated header

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOBLERTON_H
+char *leet(char *);
 char *cap_string(char *);
 char *string_toupper(char *);
 char *_strcat(char *dest, char *src);
@@ -5,3 +8,5 @@ char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+
+#endif
