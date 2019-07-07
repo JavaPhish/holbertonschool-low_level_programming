258c0f0305830a170ba54b1938ac2d8028ba861e
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-07 15:23:02 -0400

    Capitalizes the first letter of every word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,25 @@
 #include "holberton.h"
 
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @str: the string to be capitalized
+ *
+ * Return: The editted string
+ */
 char *cap_string(char *str)
 {
-	int loop, temp, isNextUpper;
+	int loop, loop1, temp, isNextUpper;
+	char wordSeps[] = {' ', '	', '\n', ',', ';', '.',
+			 '!', '?', '"', '(', ')', '{', '}'};
 
 	temp = 0;
 	isNextUpper = 0;
 	loop = 0;
+	loop1 = 0;
 	while (str[loop] != '\0')
 	{
-		if (isNextUpper == 1)
+		if (isNextUpper == 1 && str[loop] > 96 && str[loop] < 123)
 		{
 			temp = str[loop];
 			str[loop] = temp - 32;
@@ -17,24 +27,21 @@ char *cap_string(char *str)
 
 			isNextUpper = 0;
 		}
-
-
-		if (str[loop] == ','
-			|| str[loop] == ' '
-			|| str[loop] == '	'
-			|| str[loop] == '\n'
-			|| str[loop] == ';'
-			|| str[loop] == '.'
-			|| str[loop] == '!'
-			|| str[loop] == '?'
-			|| str[loop] == '"'
-			|| str[loop] == '('
-			|| str[loop] == ')'
-			|| str[loop] == '{'
-			|| str[loop] == '}')
+		else
 		{
-			isNextUpper = 1;
+			isNextUpper = 0;
 		}
+
+		loop1 = 0;
+		while (wordSeps[loop1] != '\n')
+		{
+			if (str[loop] == wordSeps[loop1])
+			{
+				isNextUpper = 1;
+			}
+		loop1++;
+		}
+
 
 		loop++;
 	}
