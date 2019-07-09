40eeb913ab5872b142e702b54b5a06f3886fbcfb
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-09 11:03:32 -0400

    Updated header

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,3 +1,5 @@
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
