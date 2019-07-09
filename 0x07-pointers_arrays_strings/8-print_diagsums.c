f65d42c1bae46aa8fde33f376daf17dcba3962b9
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-09 15:22:09 -0400

    Prints the added value of both diagonal directions in a square array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Prints the sums of both diagonals in a square array
+ * @a: The array
+ * @size: THe size of the array
+ */
+void print_diagsums(int *a, int size)
+{
+
+
+	int loop, loop2, temp, added;
+
+	loop = 0;
+	temp = 0;
+	added = 0;
+	while (loop < size * size)
+	{
+		added = added + a[loop + temp];
+		loop = loop + size;
+		temp++;
+	}
+
+	printf("%d%s", added, ", ");
+
+	added = 0;
+	while (loop > -1)
+	{
+		added = added + a[loop - temp];
+		loop = loop - size;
+		temp--;
+	}
+
+	printf("%d", added);
+	putchar('\n');
+}
