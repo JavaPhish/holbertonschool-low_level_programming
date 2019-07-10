a68cf976326f65476a46971fd2a5a081a14a3999
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-10 18:09:31 -0400

    Palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - determines if a string is a palindrome
+ * @s: the string
+ *
+ * Return: if its a palindrome
+ */
+int is_palindrome(char *s)
+{
+	return (1);
+}
