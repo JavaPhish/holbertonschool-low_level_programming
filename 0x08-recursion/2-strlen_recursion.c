caec05a13d9ee9878cfe04efaedc44251bbfbb8d
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-10 13:45:34 -0400

    Recursively finds the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Recursively finds the length string (i hate recursion)
+ * @s: The string
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
