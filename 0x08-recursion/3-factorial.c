a47faea0a99410a4d550843505b24b89bbd2d83f
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-10 13:56:46 -0400

    Recusively factors a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * factorial - Recursively returns the factorial of a number
+ * @n: The number to factor
+ *
+ * Return: The factor
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * (factorial(n - 1)));
+	}
+
+
+}
