0db84758fc865f7d38191925f1f6552246543a0a
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-14 16:19:50 -0400

    The functions in the lib

---
+++ 0x09-static_libraries/libFunction.c
@@ -0,0 +1,81 @@
+int _putchar(char c)
+{
+	return (1);
+}
+int _islower(int c)
+{
+	return (1);
+}
+int _isalpha(int c)
+{
+	return (1);
+}
+int _abs(int n)
+{
+	return (1);
+}
+int _isupper(int c)
+{
+	return (1);
+}
+int _isdigit(int c)
+{
+	return (1);
+}
+int _strlen(char *s)
+{
+	return (1);
+}
+void _puts(char *s)
+{
+
+}
+char *strcpy(char *dest, char *src)
+{
+	return ('a');
+}
+char _atoi(char *s)
+{
+	return ('a');
+}
+char *_strcat(char *dest, char *src)
+{
+	return ('a');
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return ('a');
+}
+char *strncpy(char *dest, char *src, int n)
+{
+	return ('a');
+}
+int _strcmp(char *s1, char *s2)
+{
+	return ('a');
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return ('a');
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return ('a');
+}
+char *_strchr(char *s, char c)
+{
+	return ('a');
+}
+unsigned int _strspn(char *s. char *accept)
+{
+	return (1);
+}
+char *_strpbrk(char *s, char *accept)
+{
+	return '1';
+}
+char *_strstr(char *haystack, char *needle)
+{
+	return 'a';
+
+}
