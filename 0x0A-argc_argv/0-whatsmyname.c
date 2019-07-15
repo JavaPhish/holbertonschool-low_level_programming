92ccdba7fa896acaf3908935edec039f5b00c365
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-14 22:50:10 -0400

    Prints the output name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - calls things
+ * argc: the number of commands
+ * argv: each command
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
