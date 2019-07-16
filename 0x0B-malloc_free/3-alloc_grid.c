160d5ec372c1f2a07238956c8f73b5996485e8a1
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-16 15:04:54 -0400

    makes a grid in memory

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Creates a grid of ints in mem with malloc
+ * @width: x of the grid
+ * @height: y of the grid
+ *
+ * Return: The pointer to the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr, loop;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	arr = malloc(width * sizeof(int));
+
+	for (loop = 0; loop < width; loop++)
+	{
+		arr[loop - 1] = malloc(height * sizeof(int));
+	}
+
+	return (arr);
+
+}
