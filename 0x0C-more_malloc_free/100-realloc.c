226bc4ad69e23e6a4bf0178364ecb46bc5faee35
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-19 13:54:26 -0400

    Re-allocates a pointer

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - Re allocates an already defined pointer
+ * @ptr: The original pointer
+ * @old_size: The old size
+ * @new_size: The updated size
+ *
+ * Return: The newly allocated pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	int loop;
+	char *temp;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	free(ptr);
+	temp = malloc(new_size);
+
+	for (loop = 0; loop < (int)old_size; loop++)
+	{
+		temp[loop] = ((char *)ptr)[loop];
+	}
+
+	free(ptr);
+	return (temp);
+
+}
