0060b78a8fdf2be6a70ebbde61fed631fde814cc
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-19 13:57:00 -0400

    Re-alloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,6 +16,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	int loop;
 	char *temp;
 
+	if (ptr == NULL)
+	{
+		free(ptr);
+		malloc(new_size);
+	}
+
 	if (new_size == old_size)
 		return (ptr);
 
