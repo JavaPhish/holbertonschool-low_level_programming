450a851640a362d067618a34b338f477a260a720
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 13:44:32 -0400

    Header file containing dog stuff

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
  * struct dog - a dog
  * @name: name of the dog
@@ -16,6 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
