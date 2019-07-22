ffcbe91f0596f8b5cff0d4326e9156c45a74941b
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 14:52:07 -0400

    Frees a dog_t in memory

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * free_dog - frees a dog_t type in memory
  * @d: The area in mem to free
  */
 void free_dog(dog_t *d)
 {
+	if (d)
 		free(d);
 }
