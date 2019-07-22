4618954b108fe5094a55bd55ffc3c00c753a3e2d
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 14:31:18 -0400

    Initializes a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char *_strdup(char *str);
+
 /**
  * new_dog - Returns a new dog
  * @name: The new name;
@@ -12,13 +14,61 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
-	dog_t temp = {.name = name, .age = age, .owner = owner};
-	dog_t *ret_temp = &temp;
+	char *new_name, *new_owner;
+	dog_t *ret_temp = malloc(sizeof(dog_t));
 
 	if (!ret_temp)
 		return (NULL);
 
+	new_name = _strdup(name);
+	new_owner = _strdup(owner);
+
+
+	ret_temp->name = new_name;
+	ret_temp->age = age;
+	ret_temp->owner = new_owner;
+
 	return (ret_temp);
+
+}
+
+
+/**
+ * _strdup - Duplicates a string using malloc
+ * @str: the string to duplicate
+ *
+ * Return: a pointer to the string
+ */
+char *_strdup(char *str)
+{
+	int loop, strSize;
+	char *arr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+
+
+	strSize = 1;
+	for (loop = 0; str[loop] != '\0'; loop++)
+	{
+		strSize++;
+	}
+
+	arr = malloc(strSize * (sizeof(char)));
+
+	if (!arr)
+	{
+		return (NULL);
+	}
+
+	for (loop = 0; loop < strSize; loop++)
+	{
+		arr[loop] = str[loop];
+	}
+
+	return (arr);
 
 }
