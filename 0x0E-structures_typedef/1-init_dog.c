b6505e773511ff1e1e46ef2c2fe7319dcdaae9dd
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 13:21:48 -0400

    Initialized a dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 /**
  * init_dog - intializes the dog struct with the given values
- * @dog: The struct to init
+ * @d: The struct to init
  * @name: the name of the dog
  * @age: The age of the dog
  * @owner: THe name of the owner
