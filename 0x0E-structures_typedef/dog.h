03b1ef8e1dee84295fc9fcbf70b0a1b848fe05ca
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 13:20:30 -0400

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+
+
 /**
  * struct dog - a dog
  * @name: name of the dog
@@ -14,4 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
