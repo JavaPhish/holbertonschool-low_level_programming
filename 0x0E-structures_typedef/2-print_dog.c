dda61e2fe86f14d2b1feb22126c62eea015c1cfb
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-22 13:39:54 -0400

    Prints a dog to terminal

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,44 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * print_dog - Prints a dog
  * @d: The dog to print
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+	if (d != NULL)
+	{
+
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		if (d->age < 0)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+
+
+
+	}
 }
