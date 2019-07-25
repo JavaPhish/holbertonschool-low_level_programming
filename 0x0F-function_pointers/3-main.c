f7b188e72c4b0d6f1ec786c1ccad91d24e2c392e
CARTER_CLEMENTS <831@holbertonschool.com>
2019-07-25 00:17:17 -0400

    The main for 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv)
+{
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	
+
+	return (0);
+}
