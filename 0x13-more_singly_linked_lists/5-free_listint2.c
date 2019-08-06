0368f5e108649d21a1630f18129c9340ce2bff48
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-06 15:48:42 -0400

    Frees a lis tand sets the head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+void free_listint(listint_t *head);
+
+/**
+ * free_listint2 - Sets all elements to null
+ * @head: The list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *start_node;
+
+	start_node = head->next;
+	free_listint(start_node);
+	*head = NULL;
+}
+
+
+/**
+ * free_listint - Frees a linked list entirely
+ * @head: The list to free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next_node;
+
+	while (head->next != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
+
+	free(head);
+}
