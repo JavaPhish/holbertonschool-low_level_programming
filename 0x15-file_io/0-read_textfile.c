cfabeae04d8808548e760dc14aa31832b61eb9de
CARTER_CLEMENTS <831@holbertonschool.com>
2019-08-13 17:43:45 -0400

    Reads and prints text from a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX
@@ -15,20 +15,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd, count;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	buf = malloc(letters);
 
-	if (fd < 0)
+	if (!buf)
+		return (0);
+
+	if (fd == -1)
 		return (0);
 
 	read(fd, buf, letters);
 
-	return (write(1, buf, letters));
+	for (count = 0; buf[count] != '\0'; count++)
+		_putchar(buf[count]);
+
+	return (count);
 }
 
