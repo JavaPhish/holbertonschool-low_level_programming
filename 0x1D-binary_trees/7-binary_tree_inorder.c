6bf62a7439c20ed3b2bab84a42ab4e77b7e3feaf
JavaPhish <cclements3150@gmail.com>
2019-12-04 04:04:24 +0000

    [INIT] Prints a binary tree using inorder traversal

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Outputs a binary tree using inorder traversal
+ * @tree: The tree to print
+ * @func: The function to use to print
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, (*func));
+	(*func)(tree->n);
+	binary_tree_inorder(tree->right, (*func));
+
+}
