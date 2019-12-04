23029fa0f0287f4870eca4f7d7e008e41ca5a5c7
CARTER_CLEMENTS <831@holbertonschool.com>
2019-12-04 13:56:49 -0500

    Fix

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
+	if (!new_node)
+		return (NULL);
+
 	if (parent != NULL)
 		new_node->parent = parent;
 
