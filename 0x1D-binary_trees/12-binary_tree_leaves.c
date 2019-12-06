98e58f40d7378353e9961c4b7a17ab4765ae5ac2
JavaPhish <cclements3150@gmail.com>
2019-12-06 00:52:19 +0000

    [INIT] Counts the amount of leaves in a binary tree

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	else
 		return binary_tree_leaves(tree->left) +
