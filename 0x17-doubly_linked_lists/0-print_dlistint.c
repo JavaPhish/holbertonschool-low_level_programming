700b8f718dd42e400953bcc4ca6747914759d07c
Carter C <cclements3150@gmail.com>
2020-05-01 12:27:50 -0400

    Created 0 - prints and counts the nodes in a dlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints a full doubly linked list
+ * @h: The list to print
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (node_count);
+}
