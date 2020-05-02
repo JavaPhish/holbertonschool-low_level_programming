eb558b802c7f02a9cb840e8137a8a6f658654b55
Carter C <cclements3150@gmail.com>
2020-05-02 16:37:22 -0400

    [INIT/CREATE] 5 - Gets a node at a given index or returns NULL on failure

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Gets the node at a specified index
+ * @head: The list to evaluate
+ * @index: The index to retrieve
+ * Return: The node at index, or NULL on failure (Doesnt exist, etc)
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int traverse = 0;
+
+	while (head->prev)
+		head = head->prev;
+
+	while (traverse != index)
+	{
+		traverse++;
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+
+}
