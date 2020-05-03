366103bb56e39021e0830ce86bdf91a56b83d6e4
Carter C <cclements3150@gmail.com>
2020-05-03 17:56:35 -0400

    changed format

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -11,10 +11,14 @@ def is_palindrome(num):
     return False
 
 highest = 0
+fir = 0
+sec = 0
 
 for x in range(100, 1000):
     for y in range(100, 1000):
         if (x * y > highest and is_palindrome(x * y)):
             highest = x * y
+            fir = x
+            sec = y
 
-print(highest)
+print("{} = {} x {}".format(highest, fir, sec))
