c0be3cf0435545fd2538e125576ae789ffed0412
Carter C <cclements3150@gmail.com>
2020-05-29 15:27:48 -0400

    [INIT] Creates an empty hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a base of a hash table of a set size
+ * @size: The size of the hash table to create
+ *
+ * Return: The newly created hash table, or NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
+	if (!new_table)
+		return (NULL);
+
+	new_table->array = malloc(size * sizeof(hash_node_t));
+
+	if (!new_table->array)
+		return (NULL);
+
+	new_table->size = size;
+
+	return (new_table);
+}
