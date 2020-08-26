60949c0ffbbbff64d60a3e54b1ba4bfc2664b9ef
JavaPhish <cclements3150@gmail.com>
2020-08-26 19:07:43 -0400

    Added a thing

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,6 +14,9 @@ int jump_search(int *array, size_t size, int value)
 	int gap = sqrt(size), prev = 0;
 	int srch;
 
+	if (!array)
+		return (-1);
+
 	printf("Value checked array[0] = [0]\n");
 	while (array[gap] < value && gap < (int)size)
 	{
